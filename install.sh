6647551cacbbe359e228c4ffbbfdf075a709f3fe
Rodrigo Savian <rodrigosavian@gmail.com>
2015-10-16 12:07:05 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Start install"
+sudo mkdir -p /usr/share/fonts/truetype/custom
+
+echo "Downloading font"
+wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
+
+echo "Installing font"
+sudo mv Monaco_Linux.ttf /usr/share/fonts/truetype/custom/
+
+echo "Updating font cache"
+sudo fc-cache -f -v
+
+gconftool-2 --load font-config.xml
+
+echo "Enjoy"
